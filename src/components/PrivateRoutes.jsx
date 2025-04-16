fa73a45761a99c2475adc8394ce8f5b940da6209
Micaela <micaela.zampone@jedify.io>
2025-04-16 12:13:54 -0300

    se agrega app y private routes

---
+++ src/components/PrivateRoutes.jsx
@@ -0,0 +1,10 @@
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+  const isAuthenticated = localStorage.getItem("token"); 
+  console.log(isAuthenticated);
+  
+  return isAuthenticated ? children : <Navigate to="/" />;
+};
+
+export default PrivateRoute;
